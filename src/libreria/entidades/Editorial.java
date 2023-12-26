13d5bfa75791fc74ca9b988ac521e53b054501a8
jsm notebook <jsm.juansebas@gmail.com>
2023-12-26 12:59:27 -0300

    Starting JPA project.

---
+++ src/libreria/entidades/Editorial.java
@@ -0,0 +1,83 @@
+
+package libreria.entidades;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Editorial{
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    String nombre;
+    boolean alta;
+
+    public Editorial() {
+    }
+    
+    
+    public Editorial(String id, String nombre, boolean alta) {
+        this.id = id;
+        this.nombre = nombre;
+        this.alta = alta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isAlta() {
+        return alta;
+    }
+
+    public void setAlta(boolean alta) {
+        this.alta = alta;
+    }
+    
+    
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Editorial)) {
+            return false;
+        }
+        Editorial other = (Editorial) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "libreria.entidades.Editorial[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/libreria/entidades/Libro.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package libreria.entidades;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author juansebam
+ */
+@Entity
+public class Libro {
+
+    
+    @Id
+    
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    String title;
+    int year;
+    int ejemplares;
+    int ejemplaresPrestados;
+    int ejemplaresRestantes;
+    @OneToMany
+    Autor autor;
+    @OneToMany
+    Editorial editorial;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getEjemplares() {
+        return ejemplares;
+    }
+
+    public void setEjemplares(int ejemplares) {
+        this.ejemplares = ejemplares;
+    }
+
+    public int getEjemplaresPrestados() {
+        return ejemplaresPrestados;
+    }
+
+    public void setEjemplaresPrestados(int ejemplaresPrestados) {
+        this.ejemplaresPrestados = ejemplaresPrestados;
+    }
+
+    public int getEjemplaresRestantes() {
+        return ejemplaresRestantes;
+    }
+
+    public void setEjemplaresRestantes(int ejemplaresRestantes) {
+        this.ejemplaresRestantes = ejemplaresRestantes;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Editorial getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(Editorial editorial) {
+        this.editorial = editorial;
+    }
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Libro)) {
+            return false;
+        }
+        Libro other = (Libro) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "libreria.entidades.Libro[ id=" + id + " ]";
+    }
+    
+}
